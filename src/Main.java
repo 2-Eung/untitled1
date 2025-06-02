44b226050e1583016e54931ebfa73aa82b156f25
2-Eung <eung4256@gmail.com>
2025-06-02 11:25:23 +0900

    ++--

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,14 @@ public class Main {
         int a = 10;
         int b = 3;
 
-        a += 5; // a = a + 5;
-        b -= 1; // b = b - 1;
+        System.out.println(++a);
+        System.out.println(--b);
+        System.out.println(a++);
+        System.out.println(b--);
         System.out.println(a);
         System.out.println(b);
 
-        b *= 4; // b = b * 2;
-        System.out.println(b);
-
-        b /= 3; // b = b / 2;
-        System.out.println(b);
-
-        b %= 1; // b = b % 1;
-        System.out.println(b);
+        int c = a++; // 후위연산, 할당 후 증가
+        System.out.println(c + ", " + a);
     }
 }
